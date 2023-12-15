68d17d298b18d8c502483b00c14f1c4f7192841d
pantha704 <prathamjaiswal204@gmail.com>
2023-12-15 10:49:54 +0530

    Fixed Morning Motivation mailer

--- Morning Motivation mailer/main.py
+++ Morning Motivation mailer/main.py
@@ -17,21 +17,31 @@ word_list = ["Fresh", "Super", "Amazing", "Fantastic", "Enthusiastic", "Optimist
 # day_of_week = now.weekday()
 with open("quotes.txt") as quotes:
     all_quotes = quotes.readlines()
-    with open("count.txt", mode="r+") as count:
-        quote_count = int(count.read())
-        # print("Day:", quote_count)
-    with open("count.txt", mode="w") as count:
-        if quote_count < len(all_quotes) - 1:
-            count.write(str(quote_count + 1))
-            quote = f"Day: {quote_count}\n\n{all_quotes[quote_count]}"
-        else:
-            count.write(str(0))
-            quote = (f"Day: {quote_count}\n\n{all_quotes[quote_count]}\n\nWell done! It's Day 101. Restarting from "
-                     f"tomorrow :)")
-    print(quote)
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(from_addr=my_email, to_addrs=reciever,
-#                         msg=f"Subject: Your {random.choice(word_list)} Morning Starter!\n\n{quote}")
-#     connection.close()
+    try:
+        with open("count.txt", mode="r+") as count:
+            quote_count = int(count.read())
+            # print("Day:", quote_count)
+    except:
+        with open("count.txt", mode="w") as count:
+            count.write("0")
+
+    finally:
+        with open("count.txt", mode="r+") as count:
+            quote_count = int(count.read())
+            # print("Day:", quote_count)
+            
+        with open("count.txt", mode="w") as count:
+            if quote_count < len(all_quotes) - 1:
+                count.write(str(quote_count + 1))
+                quote = f"Day: {quote_count}\n\n{all_quotes[quote_count]}"
+            else:
+                count.write(str(0))
+                quote = (f"Day: {quote_count}\n\n{all_quotes[quote_count]}\n\nWell done! It's Day 101. Restarting from "
+                         f"tomorrow :)")
+        print(quote)
+        # with smtplib.SMTP("smtp.gmail.com") as connection:
+        #     connection.starttls()
+        #     connection.login(user=my_email, password=password)
+        #     connection.sendmail(from_addr=my_email, to_addrs=reciever,
+        #                         msg=f"Subject: Your {random.choice(word_list)} Morning Starter!\n\n{quote}")
+        #     connection.close()
